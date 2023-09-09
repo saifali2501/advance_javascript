6e98e6b0fb446986b112867907334f3f34ba5c21
Saif Ali <saifalishah2501@gmail.com>
2023-09-09 00:00:02 -0700

    add

---
+++ saif/01_familytree.js
@@ -0,0 +1,43 @@
+let familytree = {
+  name: "Sagir Ahmad",
+  age: "90",
+  child: [
+    {
+      Name: "Shaukat Ali shah",
+      Age: 51,
+      occupation: "labour",
+      vehicle: [
+        {
+          Name: "spelender",
+          type: "bike",
+          Number: 786,
+        },
+      ],
+      son:[{
+        Name:"Saif ali shah",
+        
+      }],
+    },{
+
+      Name:"salim shah",
+      age:42,
+      occupation:"Labour",
+      vehicle:[{
+        Name:"thar",
+        Number:6590702,
+        type:"car",
+      }],
+      Son:[{
+        Name:"Arman ali",
+        age:20,
+        
+      },{
+        Name:"Shahbaj ali",
+        age:18,
+      }
+    
+    ],
+    },
+  ],
+};
+console.log("familytree",familytree)
